2daaeda704aef23b5f072d7b8939844ccb019d8d
Sajid <raeessajidali10@gmail.com>
2026-03-03 21:47:23 +0000

    change in routes

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/mongodb';
 import Review from '@/models/Review';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     try {
         await dbConnect();
